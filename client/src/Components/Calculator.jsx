1a5f160adb9e940f95877baf4999816340015d4e
Bhavya <bhavyasharma1401@gmail.com>
2023-03-22 18:26:09 +0530

    add:frontend

---
+++ client/src/Components/Calculator.jsx
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import LeftPanel from "./LeftPanel";
+import GraphArea from "./GraphArea";
+import ErrorPage from "./ErrorPage";
+
+function Calculator() {
+  const [monthlyInvestment, setMonthlyInvestment] = useState(500);
+  const [investmentPeriod, setInvestmentPeriod] = useState(1);
+  const [rateOfReturn, setRateOfReturn] = useState(1);
+  const [delay, setDelay] = useState(1);
+  const [startToday, setStartToday] = useState();
+  const [delayedStart, setDelayedStart] = useState();
+  const [notionalLoss, setNotionalLoss] = useState();
+  const [err, setErr] = useState();
+
+  function changeValues(index, val) {
+    switch (index) {
+      case 0:
+        setMonthlyInvestment(val);
+        break;
+      case 1:
+        setInvestmentPeriod(val);
+        break;
+      case 2:
+        setRateOfReturn(val);
+        break;
+      case 3:
+        setDelay(val);
+        break;
+    }
+  }
+
+  //For  api
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await axios.get("/getResults", {
+        params: {
+          monthlyInvestment: monthlyInvestment,
+          investmentPeriod: investmentPeriod,
+          rateOfReturn: rateOfReturn,
+          delay: delay,
+        },
+      });
+      const response = res.data.result;
+      if (res.data.status == 0) {
+        setStartToday(response.startToday);
+        setDelayedStart(response.delayedStart);
+        setNotionalLoss(response.notionalLoss);
+        setErr(false);
+      }
+      if (res.data.status == -1) {
+        setErr(true);
+      }
+    }
+    fetchData();
+  }, [monthlyInvestment, investmentPeriod, rateOfReturn, delay]);
+
+  return (
+    <div className="calculator">
+      <h2 className="heading"> SIP Delay Calculator</h2>
+
+      <h5 className="info">
+        It tells you how much wealth you can create by making monthly investment
+      </h5>
+      <div className="container">
+        <LeftPanel
+          monthlyInvestment={monthlyInvestment}
+          investmentPeriod={investmentPeriod}
+          rateOfReturn={rateOfReturn}
+          delay={delay}
+          changeValues={changeValues}
+        />
+        {err ? (
+          <ErrorPage />
+        ) : (
+          <GraphArea
+            monthlyInvestment={monthlyInvestment}
+            investmentPeriod={investmentPeriod}
+            startToday={startToday}
+            delayedStart={delayedStart}
+            notionalLoss={notionalLoss}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
---
+++ client/src/Components/LeftPanel.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import SliderArea from "./SliderArea";
+
+function leftPanel(props) {
+  return (
+    <div class="leftPanel">
+      <SliderArea
+        index={0}
+        mn={500}
+        mx={100000}
+        steps={50}
+        value={props.monthlyInvestment}
+        changeValues={props.changeValues}
+      />
+      <SliderArea
+        index={1}
+        mn={1}
+        mx={30}
+        steps={1}
+        value={props.investmentPeriod}
+        changeValues={props.changeValues}
+      />
+      <SliderArea
+        index={2}
+        mn={1}
+        mx={30}
+        steps={0.1}
+        value={props.rateOfReturn}
+        changeValues={props.changeValues}
+      />
+      <SliderArea
+        index={3}
+        mn={1}
+        mx={120}
+        steps={1}
+        value={props.delay}
+        changeValues={props.changeValues}
+      />
+    </div>
+  );
+}
+
+export default leftPanel;
